da3f0b73273b90c0b077c15923aecacb87d189c7
Kanad Shee <codewizardkanad@gmail.com>
2024-05-30 20:30:18 +0530

    Added new files

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,9 @@
 const Contact = () => {
   return (
-    <div className="h-52 w-auto mx-[200px] mt-[40px] rounded-[7px] bg-blue-500 text-md text-lime-200 text-center">
-      <h1 className="text-4xl">Contact Us</h1>
-      <h2 className="text-2xl">Gmail: abc@gmail.com</h2>
-      <h2 className="text-2xl">Mobile No: +91-0123456789</h2>
+    <div className="h-52 w-auto mx-[200px] mt-[40px] rounded-[7px] bg-blue-900 text-md text-lime-200 text-center flex items-center">
+      <h1 className="font-txtFont font-semibold text-lime-200 md:text-[40px] sm:text-[26px] m-auto">
+        Your cart is empty right now. Please add items to buy.
+      </h1>
     </div>
   );
 };
