91be2c590305375695f6a5367f60dd8bbe9c1a2e
root <root@Kanad_Shee.localdomain>
2024-07-02 13:25:23 +0530

    Added some modification

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const User = ({ name, location, contact }) => {
+  const [count, setCount] = useState(0);
   return (
     <div className="userCard mt-2">
       <h2 className="font-txtFont text-md text-teal-700 font-semibold">
@@ -10,9 +13,18 @@ const User = ({ name, location, contact }) => {
       <h4 className="font-txtFont text-sm text-cyan-800">
         Contact Info: {contact}
       </h4>
-      <button className="font-txtFont font-medium text-[12px] rounded-lg px-2 py-1 text-white my-2 bg-blue-600 hover:bg-blue-800 active:bg-white active:text-blue-800 active:border border-blue-700 hover:scale-95 duration-200">
+      <button
+        className="font-txtFont font-medium text-[12px] rounded-lg px-2 py-1 text-white my-2 bg-blue-600 hover:bg-blue-800 active:bg-white active:text-blue-800 active:border border-blue-700 hover:scale-95 duration-200"
+        onClick={() => {
+          const newCount = count + 1;
+          setCount(newCount);
+        }}
+      >
         Show more
       </button>
+      <h4 className="font-txtFont text-sm text-pink-600 p-3">
+        You have clicked this button has been clicked {count} times.
+      </h4>
     </div>
   );
 };
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,10 +3,15 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
 
   render() {
     const { name, location, contact } = this.props;
+    const { count } = this.state;
+
     return (
       <div className="userCard mt-2">
         <h2 className="font-txtFont text-md text-teal-700 font-semibold">
@@ -18,9 +23,19 @@ class UserClass extends React.Component {
         <h4 className="font-txtFont text-sm text-cyan-800">
           Contact Info: {contact}
         </h4>
-        <button className="font-txtFont font-medium text-[12px] rounded-lg px-2 py-1 text-white my-2 bg-blue-600 hover:bg-blue-800 active:bg-white active:text-blue-800 active:border border-blue-700 hover:scale-95 duration-200">
+        <button
+          className="font-txtFont font-medium text-[12px] rounded-lg px-2 py-1 text-white my-2 bg-blue-600 hover:bg-blue-800 active:bg-white active:text-blue-800 active:border border-blue-700 hover:scale-95 duration-200"
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
           Show more
         </button>
+        <h4 className="font-txtFont text-base text-pink-600 p-3">
+          This button has been clicked {count} times.
+        </h4>
       </div>
     );
   }
